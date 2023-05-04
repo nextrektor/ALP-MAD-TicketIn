349cf121f39ecce03a7ba2119c35c9702fb395c1
nale <93458378+nextrektor@users.noreply.github.com>
2023-05-04 19:40:50 +0700

    Initial Commit

---
+++ ALP MAD-TicketIn/ALP_MAD_TicketInApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MAD_TicketInApp.swift
+//  ALP MAD-TicketIn
+//
+//  Created by MacBook Pro on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MAD_TicketInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
